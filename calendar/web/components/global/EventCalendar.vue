e6fabb03ed3b20b54a3daed68ac8d57b4e4dd484
Jeffrey Fenis <jeffreyfenis@gmail.com>
2022-09-17 23:49:39 +0800

    update

--- calendar/web/components/global/EventCalendar.vue
+++ calendar/web/components/global/EventCalendar.vue
@@ -2,17 +2,15 @@
   <div :class="attr['event-calendar']">
     <div :class="attr['event-calendar__wrapper']">
       <div :class="attr['event-calendar__table-wrapper']">
-        <nav :class="attr['event-calendar__table-nav']">
-          <div>
-            <h3>{{ currentMonth.concat(' ', currentYear) }}</h3>
-            <div class="col-auto">
-              <div :class="attr['event-calendar__buttons']">
-                <button :class="attr['event-calendar__button-prev']">&#8249;</button>
-                <button :class="attr['event-calendar__button-next']">&#8250;</button>
-              </div>
-            </div>
+        <div :class="attr['event-calendar__table-nav']">
+          <div :class="attr['event-calendar__table-nav-wrapper']">
+            <h3 :class="attr['event-calendar__table-nav-title']">{{ currentMonth.concat(' ', currentYear) }}</h3>
+            <nav id="event-calendar-navigation" :class="attr['table-nav-btn']">
+              <button :class="attr['table-nav-btn-left']" data-dir="next">&#8249</button>
+              <button :class="attr['table-nav-btn-right']" data-dir="prev">&#8250</button>
+            </nav>
           </div>
-        </nav>
+        </div>
         <table :class="attr['event-calendar__table-grid']">
           <thead>
             <tr>
@@ -130,15 +128,28 @@
       ]
     }),
     methods: {
-      renderCalendar() {
+      initDateTime() {
         const dateNow = new Date()
         this.today = dateNow
         this.currentMonth = this.listOfMonths[dateNow.getMonth()]
         this.currentYear = dateNow.getFullYear()
+      },
+      populateCalendarGrid(){
+        console.log('asdfas')
+      },
+      initButtons() {
+        const calendarGridNavGrp = this.document.getElementById('event-calendar-navigation')
+        calendarGridNavGrp.children('[data-dir="prev"]')
+          .addEventListener('click', () => {
+            alert("I was pressed prev")
+            this.renderCalendar()
+        });
       }
     },
     async mounted() {
-      this.renderCalendar()
+      this.initDateTime()
+      this.initButtons()
+      this.populateCalendarGrid()
     },
   }
 </script>
@@ -166,12 +177,90 @@
         padding-right: 15px
         padding-left: 15px
       & ^[0]__table-wrapper
-        nav
+        position: relative
+        padding: 0.75rem 1.25rem
+        margin-bottom: 1rem
+        border: 1px solid transparent
+        border-radius: 0.25rem
+        & ^[0]__table-nav
           position: relative
           padding: 0.75rem 1.25rem
           margin-bottom: 1rem
           border: 1px solid transparent
           border-radius: 0.25rem
+          color: #383d41
+          background-color: #e2e3e5
+          border-color: #d6d8db
+          & ^[0]__table-nav-wrapper
+            box-sizing: border-box
+            flex-wrap: wrap
+            margin-right: -15px
+            margin-left: -15px
+            display: flex!important
+            align-items: center!important
+            & ^[0]__table-nav-title
+              display: block
+              box-sizing: border-box
+              font-family: inherit
+              font-weight: 500
+              line-height: 1.2
+              color: inherit
+              font-size: 1.75rem
+              position: relative
+              width: 100%
+              min-height: 1px
+              padding-right: 15px
+              padding-left: 15px  
+              flex-basis: 0
+              flex-grow: 1
+              max-width: 100%
+              margin: 0!important
+            & ^[0]__table-nav-btn
+              box-sizing: border-box
+              min-height: 1px
+              padding-right: 15px
+              padding-left: 15px
+              flex: 0 0 auto
+              width: auto
+              max-width: none
+              display: inline-flex
+              vertical-align: middle
+              position: relative
+              & ^[0]__table-nav-btn-left
+                align-items: flex-start
+                text-indent: 0px
+                text-shadow: none
+                letter-spacing: normal
+                word-spacing: normal
+                text-rendering: auto
+                box-sizing: border-box
+                margin: 0
+                font-family: inherit
+                overflow: visible
+                text-transform: none
+                transition: all .15s ease-in-out
+                user-select: none
+                border: 1px solid transparent
+                display: inline-block
+                font-weight: 400
+                text-align: center
+                white-space: nowrap
+                vertical-align: middle
+                color: #fff
+                background-color: #6c757d
+                border-color: #6c757d
+                padding: 0.25rem 0.5rem
+                font-size: .875rem
+                line-height: 1.5
+                border-radius: 0.2rem
+                -webkit-appearance: button
+                flex: 0 1 auto
+                position: relative
+                cursor: pointer
+
+
+
+
       & ^[0]__form-wrapper
         h4
           display: block
@@ -181,5 +270,5 @@
           line-height: 1.2
           color: inherit
           font-size: 1.5rem
-          text-align: center!important
+          text-align: center
 </style>
